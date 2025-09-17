ad374386a9e1cea4cafdb2a969a77c17fb9d3d18
Igor-Gurskiy <iixrug@yandex.ru>
2025-09-17 15:22:48 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// module.exports = {
-// 	output: 'standalone',
-// 	transpilePackages: ['react-virtualized'],
-// 	// assetPrefix: '/main'
-// }
 module.exports = {
-  output: 'standalone',
-  transpilePackages: ['react-virtualized'],
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-congress-main' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/react-congress-main' : '',
-  trailingSlash: true,
+	output: 'standalone',
+	transpilePackages: ['react-virtualized'],
+	// assetPrefix: '/main'
 }
+// module.exports = {
+//   output: 'standalone',
+//   transpilePackages: ['react-virtualized'],
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/react-congress-main' : '',
+//   basePath: process.env.NODE_ENV === 'production' ? '/react-congress-main' : '',
+//   trailingSlash: true,
+// }
