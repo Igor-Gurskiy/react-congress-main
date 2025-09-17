2eca7c9b9a20b7bd11069b4a5da8a0082492fbb4
Igor-Gurskiy <iixrug@yandex.ru>
2025-09-17 14:39:42 +0300

    Add GitHub Actions workflow for deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+// module.exports = {
+// 	output: 'standalone',
+// 	transpilePackages: ['react-virtualized'],
+// 	// assetPrefix: '/main'
+// }
 module.exports = {
-	output: 'standalone',
-	transpilePackages: ['react-virtualized'],
-	// assetPrefix: '/main'
+  output: 'standalone',
+  transpilePackages: ['react-virtualized'],
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-congress-main' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/react-congress-main' : '',
+  trailingSlash: true,
 }
